2e044722cae8d624c4e36600ca663ddca828d9db
Brian <Otto72886@m365.bfwbb-lernen.de>
2025-03-19 08:18:36 +0100

    Add functions for basic math

---
+++ script.js
@@ -0,0 +1,7 @@
+function add(num, num2) {return num + num2}
+
+function subtract(num, num2) {return num - num2}
+
+function multiply(num, num2) {return num * num2}
+
+function divide(num, num2) {return num2 != 0 ? num / num2 : "omg, no"}
