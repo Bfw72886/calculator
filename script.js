1584e4f8aed2da780a8d2d9d1da0189b3b6b0dd4
Brian <Otto72886@m365.bfwbb-lernen.de>
2025-03-19 08:30:30 +0100

    Add operate to evaluate user input

--- script.js
+++ script.js
@@ -9,3 +9,19 @@ function subtract(num, num2) {return num - num2}
 function multiply(num, num2) {return num * num2}
 
 function divide(num, num2) {return num2 != 0 ? num / num2 : "omg, no"}
+
+function operate(num, operator, num2) {
+    switch (operator) {
+        case "+":
+            return add(num, num2);
+        case "-":
+            return subtract(num, num2);
+        case "*":
+            return multiply(num, num2);
+        case "/":
+            return divide(num, num2);            
+        default:
+            console.log("Something went wrong in operate(num, operator, num2)");
+            break;
+    }
+}
